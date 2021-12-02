810b305758e4bd62710eddd30eda08d3a700f20f
Quop Murga <laurentemurga@gmail.com>
2021-12-02 17:09:06 +0000

    Finish custom filters

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from blog.models import Post
+from django.utils import timezone
 
 
 def index(request):
-  return render(request, 'blog/index.html')
+  #posts = Post.objects.filter(published_at__lte=timezone.now())
+  posts = Post.objects.all()
+  print(f'posts: {posts}')
+  return render(request, 'blog/index.html', {'posts': posts})
 
