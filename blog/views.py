2a8be0956f684a5f59e8b3e97edc2a563e9b95ee
Quop Murga <laurentemurga@gmail.com>
2021-12-03 18:12:26 +0000

    Finish custom template tags

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from blog.models import Post
 from django.utils import timezone
+from django.shortcuts import get_object_or_404
 
 
 def index(request):
@@ -9,3 +10,7 @@ def index(request):
   print(f'posts: {posts}')
   return render(request, 'blog/index.html', {'posts': posts})
 
+
+def post_detail(request, slug):
+  post = get_object_or_404(Post, slug=slug)
+  return render(request, 'blog/post-detail.html', {'post': post})
