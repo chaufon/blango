59063c9ad62414c19cb4472cf1b50d82e6cbc9e1
Quop Murga <laurentemurga@gmail.com>
2021-12-13 23:09:10 +0000

    Finish simplejwt

--- blango/settings.py
+++ blango/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+from datetime import timedelta
 from pathlib import Path
 from configurations import Configuration, values
 import dj_database_url
@@ -221,6 +222,7 @@ class Dev(Configuration):
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication"
     ],
     "DEFAULT_PERMISSION_CLASSES": [
       "rest_framework.permissions.IsAuthenticatedOrReadOnly"
@@ -244,6 +246,13 @@ class Dev(Configuration):
       'rest_framework.filters.OrderingFilter'
     ]
   }
+  
+  SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7)
+
+  }
+  
   
   SWAGGER_SETTINGS = {
       "SECURITY_DEFINITIONS": {
--- blog/api/urls.py
+++ blog/api/urls.py
@@ -5,6 +5,7 @@ from drf_yasg.views import get_schema_view
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from blog.api.views import PostList, PostDetail, UserDetail, TagViewSet, PostViewSet
 
@@ -34,7 +35,9 @@ urlpatterns = [
   re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
   path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
   path('', include(router.urls)),
-  path('posts/by-time/<str:period_name>/', PostViewSet.as_view({'get': 'list'}), name='posts-by-time')
+  path('posts/by-time/<str:period_name>/', PostViewSet.as_view({'get': 'list'}), name='posts-by-time'),
+  path('jwt/', TokenObtainPairView.as_view(), name='jwt_obtain_par'),
+  path('jwt/refresh/', TokenRefreshView.as_view(), name='jwt_refresh')
 ]
 
 #urlpatterns = format_suffix_patterns(urlpatterns)
